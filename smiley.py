3728a3f72aa180604765f53130918d59773c5ac8
Abhinandan Mishra <abhinandanm659@gmail.com>
2020-02-01 22:53:22 +0530

    Update smiley.py

--- smiley.py
+++ smiley.py
@@ -1,6 +1,11 @@
-from tkinter import *
+import tkinter as tk
+win=tk.Tk()
+win.title("SMILEY")
+canvas = tk.Canvas(win,bg="white",width=300,height=300)
+canvas.pack()
+canvas.create_oval((0,0,300,300),fill="yellow")
 
-root = Tk()
-theLable = Label(root, text="This is to easy")
-theLable.pack()
-root.mainloop()
+canvas.create_rectangle((80,90,120,130),fill="black")
+canvas.create_rectangle((190,90,230,130),fill="black")
+canvas.create_rectangle((100,200,200,220),fill="red")
+win.mainloop()
