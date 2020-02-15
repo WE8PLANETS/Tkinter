18357e73da131c544edc5baebfc45d6c62d3c38e
Abhinandan Mishra <abhinandanm659@gmail.com>
2020-02-15 11:55:27 +0530

    third_tk

---
+++ third_tk.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root= Tk()
+
+one = Label(root, text="one", bg="red", fg="white" )
+one.pack(side=LEFT)
+two = Label(root, text="two", bg="red", fg="white" )
+two.pack(side=RIGHT)
+
+
+root.mainloop()
